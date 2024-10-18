8157dae54c7c723e5f59a1adbb8c171c3b375d7c
SofiaBrizuelaa <168017026+SofiaBrizuelaa@users.noreply.github.com>
2024-10-18 20:44:44 -0300

    Add files via upload

--- elcadaverdelanovia/elcadaverdelanovia.js
+++ elcadaverdelanovia/elcadaverdelanovia.js
@@ -2,14 +2,17 @@ let estado;
 let textos = [];
 let botonA = [];
 let botonB = [];
+let imagenes = []; 
+
 
 function setup() {
-createCanvas(640, 480);
+  createCanvas(640, 480);
   inicializar();
 }
 
-
 function draw() {
+  image(imagenes[estado], 0, 0, width, height);
+
   if (estado === 0) {
     pantallaInicio();
   } else if ( estado === 100) {
@@ -22,112 +25,110 @@ function draw() {
     //pregunto por todas las pantallas con 1 boton:
   } else if ( estado===1 || estado===4 || estado===5 || estado===6 || estado===9 || estado===10 || estado===12 || estado===13 || estado===14 || estado===15 ) {
     pantallaUnBoton(textos[estado], botonA[estado]);
+    
   }
+   console.log("Mouse X: " + mouseX + ", Mouse Y: " + mouseY);
 }
 
 
 function mousePressed() {
    if (estado === 0) {
-    if ( colisionBoton(width/2, height*0.75, 200, 40) ) { //EMPEZAR
-      //flujo de estado:
+    if ( colisionBoton(width/2, height*0.75, 200, 40) ) { 
+      
       estado=1;
-    } else if ( colisionBoton(width/2, height*0.75+60, 200, 40) ) { //CREDITOS
-      //flujo de estado:
+    } else if ( colisionBoton(width/2, height*0.75+60, 200, 40) ) { 
+     
       estado=100;
     }
   } else if ( estado === 100 ) {
-    if ( colisionBoton(width/2, height*0.75+60, 200, 40) ) { //CREDITOS
-      //flujo de estado:
+    if ( colisionBoton(width/2, height*0.75+60, 200, 40) ) { 
+     
       estado=0;
     }
   } else if ( estado===1 ) {
-    if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
+    if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { //A
       //flujo de estado:
       estado=2;
      }
     } else if (estado===2 ) { 
-      if ( colisionBoton(width/2-200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+      if ( colisionBoton(width/2-200, height*0.80+60, 200, 40) ) { 
       estado=3;
-       } else if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //B
-      //flujo de estado:
+       } else if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
       estado=4;
     }
   } else if ( estado===3 ) {
-    if ( colisionBoton(width/2-200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+    if ( colisionBoton(width/2-200, height*0.80+60, 200, 40) ) { 
+      
       estado=5;
-      } else if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //B
-      //flujo de estado:
+      } else if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+  
       estado=6;
     }
   } else if ( estado===4 ) {
-    if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+    if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
       estado=8;
     }
   } else if ( estado===5 ) {
-  if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+  if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+    
       estado=7;
    }
   } else if ( estado===6 ){
-  if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+  if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+      
       estado=7;
    }
   } else if ( estado===7 ){
-     if ( colisionBoton(width/2-200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+     if ( colisionBoton(width/2-200, height*0.80+60, 200, 40) ) {
+      
       estado=11;
-      } else if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //B
-      //flujo de estado:
+      } else if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) {
+      
       estado=12;
       }
   } else if ( estado===8 ){
-     if ( colisionBoton(width/2-200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+     if ( colisionBoton(width/2-200, height*0.80+60, 200, 40) ) { 
+  
       estado=9;
-      } else if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //B
-      //flujo de estado:
+      } else if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+     
       estado=10;
       }
   } else if ( estado===9 ){
-     if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+     if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+    
       estado=3;
      }
   } else if ( estado===10 ){
-    if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+    if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+  
       estado=15;
     }
   } else if ( estado===11 ){
-     if ( colisionBoton(width/2-200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+     if ( colisionBoton(width/2-200, height*0.80+60, 200, 40) ) { 
+      
       estado=13;
-      } else if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //B
-      //flujo de estado:
+      } else if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+      
       estado=14;
       }
   }  else if ( estado===12 ){
-      if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+      if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
+      
       estado=10;
       }
   } else if ( estado===13 ){
-     if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
+     if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) { 
       //flujo de estado:
       estado=0;
      }
   } else if ( estado===14 ){
-    if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+    if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) {
+      
       estado=0;
     }
   } else if ( estado===15 ){
-        if ( colisionBoton(width/2+200, height*0.75+60, 200, 40) ) { //A
-      //flujo de estado:
+        if ( colisionBoton(width/2+200, height*0.80+60, 200, 40) ) {
       estado=0;
     }
   }
